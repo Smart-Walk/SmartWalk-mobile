465a5d46774798b89a8da1cca8c06541b9375daa
SilentCruzer <radithyakumar7@gmail.com>
2023-10-31 20:15:07 +0530

    Adds learn functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:wifi_scan/wifi_scan.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -31,6 +37,8 @@ class _GameGridState extends State<GameGrid> {
   final double cellSize = 15.0;
   int selectedGrid = -1;
   List<WiFiAccessPoint> accessPoints = <WiFiAccessPoint>[];
+  String selectedSSID = "";
+  int selectedSignal = 0;
 
   void kShowSnackBar(BuildContext context, String message) {
     if (kDebugMode) print(message);
@@ -40,11 +48,11 @@ class _GameGridState extends State<GameGrid> {
   }
 
   Future<void> _startScan(BuildContext context) async {
-      final can = await WiFiScan.instance.canStartScan();
-      if (can != CanStartScan.yes) {
-        if (mounted) kShowSnackBar(context, "Cannot start scan: $can");
-        return;
-      }
+    final can = await WiFiScan.instance.canStartScan();
+    if (can != CanStartScan.yes) {
+      if (mounted) kShowSnackBar(context, "Cannot start scan: $can");
+      return;
+    }
 
     final result = await WiFiScan.instance.startScan();
     if (mounted) kShowSnackBar(context, "startScan: $result");
@@ -52,90 +60,129 @@ class _GameGridState extends State<GameGrid> {
   }
 
   Future<bool> _canGetScannedResults(BuildContext context) async {
-      final can = await WiFiScan.instance.canGetScannedResults();
-      // if can-not, then show error
-      if (can != CanGetScannedResults.yes) {
-        if (mounted) kShowSnackBar(context, "Cannot get scanned results: $can");
-        accessPoints = <WiFiAccessPoint>[];
-        return false;
-      }
+    final can = await WiFiScan.instance.canGetScannedResults();
+    // if can-not, then show error
+    if (can != CanGetScannedResults.yes) {
+      if (mounted) kShowSnackBar(context, "Cannot get scanned results: $can");
+      accessPoints = <WiFiAccessPoint>[];
+      return false;
+    }
     return true;
   }
-
 
   Future<void> _getScannedResults(BuildContext context) async {
     if (await _canGetScannedResults(context)) {
       final results = await WiFiScan.instance.getScannedResults();
-      print(results[0].bssid);
+      final amritaConnectAccessPoints = results.where((ap) => ap.ssid == "AMRITA-Connect").toList();
+
+      final jsonData = {
+        "location": selectedGrid,
+        "access_points": {
+          for (int i = 0; i < amritaConnectAccessPoints.length; i++)
+            '${amritaConnectAccessPoints[i].ssid} ${amritaConnectAccessPoints[i].bssid}': amritaConnectAccessPoints[i].level,
+        }
+      };
+
+      showPopup(jsonData);
       setState(() => accessPoints = results);
     }
   }
 
-  @override
-  void initState() {
-    super.initState();
+  void showPopup(Map<String, Object>  data) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("WiFi Information"),
+          content: Text("Wifi data: \n$data"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Close"),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return InteractiveViewer(
-      child: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/images/hostel.png"),
-              fit: BoxFit.fill,
-            ),
-          ),
-          child: Column(
-            children: List.generate(
-              rows,
-                  (row) => Expanded(
-                child: Row(
-                  children: List.generate(
-                    columns,
-                        (col) {
-                      final gridNumber = row * columns + col;
-                      return GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            if (selectedGrid != gridNumber) {
-                              selectedGrid = gridNumber;
-                              _startScan(context);
-                              _getScannedResults(context);
-                            } else {
-                              // If the same grid is tapped again, deselect it.
-                              selectedGrid = -1;
-                            }
-                          });
+    return Scaffold(
+      body: Stack(
+        children: [
+          InteractiveViewer(
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/hostel.png"),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              child: Column(
+                children: List.generate(
+                  rows,
+                      (row) => Expanded(
+                    child: Row(
+                      children: List.generate(
+                        columns,
+                            (col) {
+                          final gridNumber = row * columns + col;
+                          return GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                if (selectedGrid != gridNumber) {
+                                  selectedGrid = gridNumber;
+                                } else {
+                                  // If the same grid is tapped again, deselect it.
+                                  selectedGrid = -1;
+                                }
+                              });
+                            },
+                            child: GridTile(
+                              child: Container(
+                                width: cellSize,
+                                height: cellSize,
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Colors.grey,
+                                    width: 0.1,
+                                  ),
+                                  color: selectedGrid == gridNumber
+                                      ? Colors.green
+                                      : null,
+                                ),
+                                child: Center(
+                                  child: Text("."),
+                                ),
+                              ),
+                            ),
+                          );
                         },
-                        child: GridTile(
-                          child: Container(
-                            width: cellSize,
-                            height: cellSize,
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Colors.grey,
-                                width: 0.1,
-                              ),
-                              color:
-                              selectedGrid == gridNumber ? Colors.green : null,
-                            ),
-                            child: Center(
-                              child: Text(
-                                ".",
-                              ),
-                            ),
-                          ),
-                        ),
-                      );
-                    },
+                      ),
+                    ),
                   ),
                 ),
               ),
             ),
           ),
-        ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: FloatingActionButton(
+                backgroundColor: Colors.brown,
+                onPressed: () {
+                  _startScan(context);
+                  _getScannedResults(context);
+                },
+                child: Icon(Icons.wifi),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
